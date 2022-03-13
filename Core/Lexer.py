00147634ba722d93cf49df328677437b6ec70c62
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-13 21:57:40 +0530

    add: added constant importer for python

--- Core/Lexer.py
+++ Core/Lexer.py
@@ -1,3 +1,5 @@
+from Core.constants import *
+
 #######################################
 # CONSTANTS
 #######################################
@@ -55,16 +57,6 @@ class Position:
 #######################################
 # TOKENS
 #######################################
-
-
-TT_INT = 'INT'
-TT_FLOAT = 'FLOAT'
-TT_PLUS = 'PLUS'
-TT_MINUS = 'MINUS'
-TT_MUL = 'MUL'
-TT_DIV = 'DIV'
-TT_LPAREN = 'LPAREN'
-TT_RPAREN = 'RPAREN'
 
 
 class Token:
