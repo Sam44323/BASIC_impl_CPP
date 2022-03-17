160f95778a0b7c1fb89a59f02c546425a74c2ec1
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-17 21:17:30 +0530

    add: added multi-token support

--- Core/interpreter.py
+++ Core/interpreter.py
@@ -1,140 +1,124 @@
-# Token constant definition
+# Token types
 #
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
+INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
 
 
-PLUS, MINUS, MULTI, DIV, EOF, INTEGER = 'PLUS', 'MINUS', 'MULTI', 'DIV', 'EOF', 'INTEGER'
+class Token(object):
+    def __init__(self, type, value):
+        # token type: INTEGER, PLUS, MINUS, or EOF
+        self.type = type
+        # token value: non-negative integer value, '+', '-', or None
+        self.value = value
 
-
-class Token:
-    def __init__(self, type, value) -> None:
-        self.type = type  # INTEGER, PLUS, or EOF
-        self.value = value  # value: 0, 1, 2. 3, 4, 5, 6, 7, 8, 9, '+', or None
-
-    def __repr__(self) -> str:
+    def __str__(self):
         """String representation of the class instance.
 
         Examples:
             Token(INTEGER, 3)
-            Token(PLUS '+')
+            Token(PLUS, '+')
         """
         return 'Token({type}, {value})'.format(
             type=self.type,
             value=repr(self.value)
         )
 
+    def __repr__(self):
+        return self.__str__()
 
-class Interpreter:
-    def __init__(self, text) -> None:
-        self.text = text  # example: 10 + 5
-        self.pos = 0  # the current position in the text
-        self.current_token = None  # the current token
 
+class Interpreter(object):
+    def __init__(self, text):
+        # client string input, e.g. "3 + 5", "12 - 5 + 3", etc
+        self.text = text
+        # self.pos is an index into self.text
+        self.pos = 0
+        # current token instance
+        self.current_token = None
+        self.current_char = self.text[self.pos]
+
+    ##########################################################
+    # Lexer code                                             #
+    ##########################################################
     def error(self):
-        raise Exception('Error parsing input')
+        raise Exception('Invalid syntax')
 
-    def token_advancer(self):
-        """Lexical analyzer
-
-          This method is responsible for breaking a sentence
-          apart into tokens. One token at a time.
-        """
-        text = self.text
-
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
-
-        curr_char = text[self.pos]
-
-        if curr_char.isdigit():
-            token = Token(INTEGER, int(curr_char))
-            self.pos += 1
-            return token
-
-        elif curr_char == '+':
-            token = Token(PLUS, curr_char)
-            self.pos += 1
-            return token
-
-        elif curr_char == '-':
-            token = Token(MINUS, curr_char)
-            self.pos += 1
-            return token
-
-        elif curr_char == '*':
-            token = Token(MULTI, curr_char)
-            self.pos += 1
-            return token
-        elif curr_char == '/':
-            token = Token(DIV, curr_char)
-            self.pos += 1
-            return token
-
-        elif curr_char == ' ':
-            self.pos += 1
-            return self.token_advancer()
-
+    def advance(self):
+        """Advance the `pos` pointer and set the `current_char` variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
         else:
-            return self.error()   # throw error if none of the condition matches the curr_char
+            self.current_char = self.text[self.pos]
 
-    def token_matcher(self, token_type):
-       # compare the current token type with the passed token
-       # type and if they match then replace the current token
-       # and assign the next token to the self.current_token,
-       # otherwise raise an exception
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
 
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        """Lexical analyzer (also known as scanner or tokenizer)
+
+        This method is responsible for breaking a sentence
+        apart into tokens. One token at a time.
+        """
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            self.error()
+
+        return Token(EOF, None)
+
+    ##########################################################
+    # Parser / Interpreter code                              #
+    ##########################################################
+    def token_advancer(self, token_type):
         if self.current_token.type == token_type:
-            self.current_token = self.token_advancer()
+            self.current_token = self.get_next_token()
         else:
             self.error()
 
-    def expression(self):
-        """expression -> INTEGER PLUS/MINUS/MULTI/DIVIDE INTEGER"""
+    def term(self):
+        """Return an INTEGER token value."""
+        token = self.current_token
+        self.token_advancer(INTEGER)
+        return token.value
 
-        (left, right) = (0, 0)
+    def expr(self):
+        """Arithmetic expression parser / interpreter."""
+        # set current token to the first token taken from the input
+        self.current_token = self.get_next_token()
 
-        # setting current token to the first token taken from the input
-        self.current_token = self.token_advancer()
-
-        while self.current_token.type == INTEGER:
-            try:
-                left = left * 10 + int(self.current_token.value)
-                self.token_matcher(INTEGER)
-            except:
-                break
-
-        # we expect the current token to be a '+' or '-' or '*' token
-
-        op = self.current_token
-        if op.type == PLUS:
-            self.token_matcher(PLUS)
-        elif op.type == MINUS:
-            self.token_matcher(MINUS)
-        elif op.type == MULTI:
-            self.token_matcher(MULTI)
-        elif op.type == DIV:
-            self.token_matcher(DIV)
-
-        # we expect the current token to be a single-digit/multi-digit integer
-
-        while self.current_token.type == INTEGER:
-            try:
-                right = right * 10 + int(self.current_token.value)
-                self.token_matcher(INTEGER)
-            except:
-                break
-
-        # now the self.current_token is set to EOF token
-        if op.type == PLUS:
-            result = left + right
-        elif op.type == MINUS:
-            result: int = left - right
-        elif op.type == MULTI:
-            result = left * right
-        elif op.type == DIV:
-            result = left / right
-
-        print("Result: ", result)
+        result = self.term()
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.token_advancer(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.token_advancer(MINUS)
+                result = result - self.term()
 
         return result
--- terminal.py
+++ terminal.py
@@ -11,7 +11,7 @@ def main():
         if not text:
             continue
         interpreter = Interpreter(text)
-        result = interpreter.expression()
+        result = interpreter.expr()
         print(result)
 
 
