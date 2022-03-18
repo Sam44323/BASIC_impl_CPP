79c47fa900434a60aa4ae752d7ad4a477e1bf974
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-18 19:35:49 +0530

    add: added custom class abstraction for Lexer

--- Core/interpreter.py
+++ Core/interpreter.py
@@ -27,6 +27,72 @@ class Token(object):
 ##########################################################
 
 
+class Lexer:
+    def __init__(self, text) -> None:
+        # client string input, e.g. "3 + 5", "12 - 5 + 3", etc
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Invalid syntax')
+
+    def token_advancer(self):
+        """Advances the `pos` pointer and set the `current_char` variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.token_advancer()
+
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.token_advancer()
+        return int(result)
+
+    def get_next_token(self):
+        """Lexical analyzer (also known as scanner or tokenizer)
+
+        This method is responsible for breaking a sentence
+        apart into tokens. One token at a time.
+        """
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.token_advancer()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.token_advancer()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.token_advancer()
+                return Token(MULTI, '*')
+
+            if self.current_char == '/':
+                self.token_advancer()
+                return Token(DIV, '/')
+
+            self.error()
+
+        return Token(EOF, None)
+
+
 ##########################################################
 # Parser / Interpreter code                              #
 ##########################################################
